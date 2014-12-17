502e443875fec711cbb31bb2d95b16375684fab8
Quentin PEREZ <quentin.perez@edu.univ-fcomte.fr>
2014-12-17 19:05:47 +0100

    Ajout d'images pour les blocs et modifications du CSS perso

--- holoprog-codeIgniter/application/views/clients/exo_1.php
+++ holoprog-codeIgniter/application/views/clients/exo_1.php
@@ -12,16 +12,32 @@
             <section id="connected">
                 <div class="col-md-4 list-reponse">
                     <ul class="connected list">
-                        <li><SELECT class="form-control" name="0" >
-                                <OPTION value="1">Ouvrir la vanne amont</OPTION>
-                                <OPTION value="2">Ouvrir la porte amont</OPTION>
-                                <OPTION value="3">3</OPTION>
-                                <!--<OPTION value="1">1</OPTION>
+                        <li><img src="<?php echo base_url();?>img/bloc_debut.png"></li>
+
+                        <div class="row"></div>
+                        <li>
+                            <SELECT class="form-control" name="4" size="1">
+                                <OPTION value="1">1</OPTION>
                                 <OPTION value="2">2</OPTION>
                                 <OPTION value="3">3</OPTION>
                                 <OPTION value="4">4</OPTION>
-                                <OPTION value="5">5</OPTION>-->
-                            </SELECT></li>
+                                <OPTION value="5">5</OPTION>
+                            </SELECT>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4 list-question">
+                    <ul class="connected list no2">
+                        <li><div class="bloc_condition"><SELECT class="" name="0" >
+                                    <OPTION value="1">Ouvrir la vanne amont</OPTION>
+                                    <OPTION value="2">Ouvrir la porte amont</OPTION>
+                                    <OPTION value="3">3</OPTION>
+                                    <!--<OPTION value="1">1</OPTION>
+                                    <OPTION value="2">2</OPTION>
+                                    <OPTION value="3">3</OPTION>
+                                    <OPTION value="4">4</OPTION>
+                                    <OPTION value="5">5</OPTION>-->
+                                </SELECT></div></li>
 
                         <li><SELECT class="form-control" name="1" size="1">
                                 <OPTION value="1">1</OPTION>
@@ -51,27 +67,6 @@
                                 <OPTION value="5">5</OPTION>
                             </SELECT></li>
 
-                        <div class="row"></div>
-                        <li>
-                            <SELECT class="form-control" name="4" size="1">
-                                <OPTION value="1">1</OPTION>
-                                <OPTION value="2">2</OPTION>
-                                <OPTION value="3">3</OPTION>
-                                <OPTION value="4">4</OPTION>
-                                <OPTION value="5">5</OPTION>
-                            </SELECT>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-4 list-question">
-                    <ul class="connected list no2">
-                        <li class="highlight">Item 1</li>
-                        <li class="highlight">Item 2</li>
-                        <li class="highlight">Item 3</li>
-                        <li class="highlight">Item 4</li>
-                        <li class="highlight">Item 5</li>
-                        <li class="highlight">Item 6</li>
-                    </ul>
                 </div>
             </section>
             <div class="row"></div>
