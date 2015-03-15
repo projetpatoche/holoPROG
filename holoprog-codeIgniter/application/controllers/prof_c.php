ad5b5b95b823cedc677b160c39f28f529002c3d1
asauner <alex.sauner@orange.fr>
2015-03-15 17:19:26 +0100

    Update prof_c.php

--- holoprog-codeIgniter/application/controllers/prof_c.php
+++ holoprog-codeIgniter/application/controllers/prof_c.php
@@ -53,6 +53,61 @@ class Prof_c extends CI_Controller {
         $this->load->view('users_apropos');
         $this->load->view('prof/prof_foot');
     }
+    
+     public function ajoutExercice(){
+        $this->load->view('prof/prof_head');
+
+        $data['listeClasses']=$this->prof_m->getClasses($this->session->userdata('id_professeur'));
+        $this->load->view('prof/prof_menu',$data);
+        $this->load->view('prof/prof_ajout');
+        $this->load->view('prof/prof_foot');
+    }
+
+    public function ajoutImage(){
+        $auto_int = $this->prof_m->autoIntExercice();
+        mkdir ("img/exercice/".$auto_int['auto_increment']);
+        $this->load->view('prof/prof_head');
+        $url = dirname(dirname(dirname(__FILE__)))."/img/exercice/".$auto_int['auto_increment']."/".$_FILES['icone']['name'];
+        $url = str_replace( "\\","/" , $url);
+        $urlenonce = dirname(dirname(dirname(__FILE__)))."/img/exercice/".$auto_int['auto_increment']."/".$_FILES['enonce']['name'];
+        $urlenonce = str_replace( "\\","/" , $urlenonce);
+        $urlenonceBDD="../../../img/exercice/".$auto_int['auto_increment']."/".$_FILES['enonce']['name'];
+        $fn = @imagecreatefromjpeg( $_FILES['enonce']['tmp_name'] );
+        imagejpeg($fn, $urlenonce);
+
+        $fn = @imagecreatefromjpeg( $_FILES['icone']['tmp_name'] );
+        $nbSelect=$_POST['nbSelect'];
+        $x = imagesx($fn);
+        $y = imagesy($fn)/$nbSelect;
+        $result="";
+        for($i=1;$i<=$nbSelect;$i++){
+            $number=$_POST[($i)];
+            if($i!=$nbSelect)
+                $result =$result . $number . '-';
+            else
+                $result = $result . $number;
+        }
+        $this->prof_m->insertExercice($result,$nbSelect,$_POST['difficulte'],$urlenonceBDD);
+
+        /*
+        decoupage de l'image
+        */
+
+        for($i=0;$i<$nbSelect;$i++){
+            $im = @imagecreatetruecolor( $x, $y );
+            $true = imagecopyresized( $im, $fn, 0, 0, 0, $i * $y, $x, $y, $x, $y );
+            $url= dirname(dirname(dirname(__FILE__)))."/img/exercice/".$auto_int['auto_increment']."/".$i.".jpeg";
+            $urlBDD="../../../img/exercice/".$auto_int['auto_increment']."/".$i.".jpeg";
+            imagejpeg($im, $url);// enregistrement de l'image
+
+            $proposition=$_POST[(($i+1)*100)+1]."__".$_POST[(($i+1)*100)+2]."__".$_POST[(($i+1)*100)+3];
+            $this->prof_m->insertProposition($proposition,$urlBDD);
+        }
+
+        $data['listeClasses']=$this->prof_m->getClasses($this->session->userdata('id_professeur'));
+        $this->load->view('prof/prof_menu',$data);
+        $this->load->view('prof/prof_foot');
+    }
 
 
 }
