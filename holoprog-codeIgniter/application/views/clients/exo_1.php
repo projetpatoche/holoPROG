9966036f0fcff618ac0132e517e6385c366627df
asauner <alex.sauner@orange.fr>
2015-03-15 18:03:57 +0100

    Update exo_1.php

--- holoprog-codeIgniter/application/views/clients/exo_1.php
+++ holoprog-codeIgniter/application/views/clients/exo_1.php
@@ -5,7 +5,7 @@
 <div class = "row">
     <div class="col-md-3"></div>
     <div class="col-md-3">
-        <img src="<?php echo base_url();?>img/exercice/barriere/barriere_consignes.jpg">
+        <img src="<?php echo $exercice->image_enonce?>">
     </div>
     
      <div class="col-md-6">
