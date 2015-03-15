8c20e4ac0244aceef5853092742cd752da93c758
asauner <alex.sauner@orange.fr>
2015-03-15 17:32:47 +0100

    Update exo_1.php

--- holoprog-codeIgniter/application/views/clients/exo_1.php
+++ holoprog-codeIgniter/application/views/clients/exo_1.php
@@ -7,43 +7,27 @@
     <div class="col-md-3">
         <img src="<?php echo base_url();?>img/exercice/barriere/barriere_consignes.jpg">
     </div>
-    <div class="col-md-6">
-        <form method="post" action="<?php echo base_url();?>index.php/client_c/correctionExo/1" >
+    
+     <div class="col-md-6">
+        <form method="post" action="<?php echo base_url();?>index.php/client_c/correctionExo/<?php echo $exercice->id_exercice?> " >
             <div class="">
-                <div class="barriere_bloc_1">
-                    <SELECT class="select_barriere_bloc_1" name="0">
-                        <OPTION value="1">Début</OPTION>
-                        <OPTION value="2">Fin</OPTION>
-                        <OPTION value="3">Démarrage</OPTION>
-                    </SELECT>
-                </div>
-
-                <div class="barriere_bloc_2">
-                    <SELECT class="select_barriere_bloc_2" name="1" >
-                        <OPTION value="1">Code valide ?</OPTION>
-                        <OPTION value="2">Code invalide ?</OPTION>
-                        <OPTION value="3">Ouverture</OPTION>
-                    </SELECT></div></li>
-
-                <div class="barriere_bloc_3">
-                    <SELECT class="select_barriere_bloc_3" name="2" >
-                        <OPTION value="1">Ouvrir barrière + allumer voyant</OPTION>
-                        <OPTION value="2">Fermer barrière</OPTION>
-                        <OPTION value="3">Allumer le voyant</OPTION>
-                    </SELECT></div>
-                <div class="barriere_bloc_4">
-                    <SELECT class="select_barriere_bloc_4" name="3" >
-                        <OPTION value="1">Allumer voyant</OPTION>
-                        <OPTION value="2">Voiture passée ?</OPTION>
-                        <OPTION value="3">Fermer barrière</OPTION>
-                    </SELECT></div>
-                <div class="barriere_bloc_5">
-                    <SELECT class="select_barriere_bloc_5" name="4" >
-                        <OPTION value="1">Voiture passée</OPTION>
-                        <OPTION value="2">Eteindre voyant</OPTION>
-                        <OPTION value="3">Fermer barrière + éteindre voyant</OPTION>
-                    </SELECT></div>
-
+			<?php if($exercice->difficulte==2){?>
+			<ul class="sortable">
+			<?php } ?>
+				<?php for($i=0;$i<$exercice->taille_exo;$i++){?>
+					<div class="barriere_bloc_1">
+						<SELECT class="select_barriere_bloc_1" name="<?php echo $i ?>">
+							<OPTION value="1"><?php echo $select[''.$i.''][0]?></OPTION>
+							<OPTION value="2"><?php echo $select[''.$i.''][1]?></OPTION>
+							<OPTION value="3"><?php echo $select[''.$i.''][2]?></OPTION>
+						</SELECT>
+						<img src="<?php echo $select[''.$i.'']['img']?>">
+						
+					</div>
+				<?php } ?>
+			<?php if($exercice->difficulte==2){?>
+			</ul>
+			<?php } ?>
             </div>
 
             <div class="row"></div>
@@ -55,6 +39,7 @@
         </form>
     </div>
 </div>
+
 <script>
     $(function() {
         $('.sortable').sortable();
