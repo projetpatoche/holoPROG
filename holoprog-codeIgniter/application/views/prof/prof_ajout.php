ff53a121146ed9e884893a8e4ec134b7fbcd65f9
asauner <alex.sauner@orange.fr>
2015-03-18 16:40:24 +0100

    Update prof_ajout.php

--- holoprog-codeIgniter/application/views/prof/prof_ajout.php
+++ holoprog-codeIgniter/application/views/prof/prof_ajout.php
@@ -11,21 +11,31 @@
 		select="";
 		
 		for(i=1;i<=nb;i++){
-			select+='Select '+i+' : </br><input type="text" name="'+i+'01"/></br><input type="text" name="'+i+'02"/></br><input type="text" name="'+i+'03"/></br>';
+			select+='Champ '+i+' : </br><input type="text" name="'+i+'01"/><br /><input type="text" name="'+i+'02"/><br /><input type="text" name="'+i+'03"/></br>';
 			select+='<INPUT type= "radio" name="'+i+'" value="1" checked> 1<INPUT type= "radio" name="'+i+'" value="2"> 2<INPUT type= "radio" name="'+i+'" value="3"> 3</br></br>';
 		}
 		document.getElementById("select").innerHTML=select;
 	}
 </script>
-<form method="post" action="<?php echo base_url();?>index.php/prof_c/ajoutImage" enctype="multipart/form-data">
-	Nombre de select : <input type="text" id="nb" value="1" name="nbSelect" pattern="[0-9]{1,2}" onkeyup="ajoutSelect(this.value)"/><br />
-	Image des exercices : <input type="file" name="icone" /><br />
-	Image d'enonce : <input type="file" name="enonce" /><br />
-	difficulte : <select name="difficulte" >
+<form method="post" class="ajout" action="<?php echo base_url();?>index.php/prof_c/ajoutImage" enctype="multipart/form-data">
+	Nombre de champ<br /><input type="text" id="nb" value="1" name="nbSelect" pattern="[0-9]{1,2}" onkeyup="ajoutSelect(this.value)"/><br /><br />
+	Image pour l'exercice<input type="file" name="icone" /><br />
+	Image pour l'enonce<input type="file" name="enonce" /><br />
+	difficulte <br /><select name="difficulte" >
 		<OPTION value="1">1</OPTION>
 		<OPTION value="2">2</OPTION>
 		<OPTION value="3">3</OPTION>
-	</select>
-	<div id="select"></div>
-	<button type="submit" name="act_soumettre" value="Valide"/> Valider</button>
+	</select><br /><br />
+	
+	<div id="select">Champ 1 : <br />
+	<input type="text" name="101"/><br />
+	<input type="text" name="'102"/><br />
+	<input type="text" name="103"/><br />
+	
+	<INPUT type= "radio" name="1" value="1" checked> 1
+	<INPUT type= "radio" name="1" value="2"> 2
+	<INPUT type= "radio" name="1" value="3"> 3</div>
+		
+	<br /><button class="btn-success btn btn-large btn-block btn-primary" type="submit" name="act_soumettre" value="Valide"/> Valider</button>
+</form>
 </form>
