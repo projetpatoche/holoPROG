a44aa8eca472e369afa20a0de9060fab12570c36
reikka <serahreikka@reikka-design.com>
2015-03-19 17:21:08 +0100

    Ajout de l'inscription complète d'un élève, attention précautions à prendre (cf Fb)

--- holoprog-codeIgniter/application/views/users_menu.php
+++ holoprog-codeIgniter/application/views/users_menu.php
@@ -27,7 +27,7 @@
                 </a>
             </li>
             <li >
-                <a href="mailto:patochee@patoche.com">
+                <a href="mailto:patoche@patoche.com">
 								<span class="icon">
 									<i aria-hidden="true" class="icon-contact"></i>
 								</span>
