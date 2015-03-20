cf2b6937a483e7605d692b3edfbc87d271419f54
asauner <alex.sauner@orange.fr>
2015-03-20 19:14:02 +0100

    Update exo_1.php
    mélange pour la difficulté 2

--- holoprog-codeIgniter/application/views/clients/exo_1.php
+++ holoprog-codeIgniter/application/views/clients/exo_1.php
@@ -11,15 +11,19 @@
      <div class="col-md-6">
         <form method="post" action="<?php echo base_url();?>index.php/client_c/correctionExo/<?php echo $exercice->id_exercice?> " >
             <div class="">
-			<?php if($exercice->difficulte==2){?>
+			<?php for($i=0;$i<$exercice->taille_exo;$i++){//liste pour le melange
+				$liste[]=$i;
+			}
+			if($exercice->difficulte==2){
+				shuffle($liste);?>
 			<ul class="sortable">
 			<?php } ?>
 				<?php for($i=0;$i<$exercice->taille_exo;$i++){?>
 					<div class="barriere_bloc_1">
-						<SELECT class="select_barriere_bloc_1" name="<?php echo $i ?>">
-							<OPTION value="1"><?php echo $select[''.$i.''][0]?></OPTION>
-							<OPTION value="2"><?php echo $select[''.$i.''][1]?></OPTION>
-							<OPTION value="3"><?php echo $select[''.$i.''][2]?></OPTION>
+						<SELECT class="select_barriere_bloc_1" name="<?php echo $liste[$i] ?>">
+							<OPTION value="1"><?php echo $select[''.$liste[$i].''][0]?></OPTION>
+							<OPTION value="2"><?php echo $select[''.$liste[$i].''][1]?></OPTION>
+							<OPTION value="3"><?php echo $select[''.$liste[$i].''][2]?></OPTION>
 						</SELECT>
 						<img src="<?php echo $select[''.$i.'']['img']?>">
 						
